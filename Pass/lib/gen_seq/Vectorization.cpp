3c66b178c0cd21061e24f32854df9a0b7ddf9d8a
Meng Sun <xunm@umich.edu>
2025-04-22 01:16:32 -0400

    feat: win

--- Pass/lib/gen_seq/Vectorization.cpp
+++ Pass/lib/gen_seq/Vectorization.cpp
@@ -17,8 +17,16 @@
 #include "llvm/Support/Casting.h"
 #include "llvm/Support/raw_ostream.h"
 #include "Hello/HelloPasses.h"
+#include <cstddef>
 #include <unordered_map>
 #include <vector>
+
+void collect_garbage(std::vector<mlir::Operation *> &garbage) {
+    while (!garbage.empty()) {
+        garbage.back()->erase();
+        garbage.pop_back();
+    }
+}
 
 void vectorize_ops(std::unordered_map<mlir::Operation *, mlir::OpResult> &map, std::vector<mlir::Operation *> &garbage, mlir::Operation *op, mlir::OpBuilder &builder) {
     if (auto loadOp = mlir::dyn_cast<mlir::affine::AffineLoadOp>(op)) {
@@ -27,7 +35,8 @@ void vectorize_ops(std::unordered_map<mlir::Operation *, mlir::OpResult> &map, s
         auto indices = loadOp.getIndices();
         const auto &loadOp_vec = builder.create<mlir::vector::LoadOp>(loadOp->getLoc(), vectorType, memRef, indices);
         map[loadOp.getOperation()] = loadOp_vec->getResult(0);
-        // TODO: collect garbage
+        // collect garbage
+        garbage.push_back(loadOp.getOperation());
     } else if (auto addiOp = mlir::dyn_cast<mlir::arith::AddIOp>(op)) {
         auto op1 = addiOp.getOperand(0);
         auto op2 = addiOp.getOperand(1);
@@ -35,33 +44,44 @@ void vectorize_ops(std::unordered_map<mlir::Operation *, mlir::OpResult> &map, s
         auto op2_new = map[op2.getDefiningOp()];
         const auto &addi_vec = builder.create<mlir::arith::AddIOp>(addiOp->getLoc(), op1_new, op2_new);
         map[addiOp.getOperation()] = addi_vec->getResult(0);
-        // TODO: collect garbage
+        // collect garbage
+        garbage.push_back(addiOp.getOperation());
     } else if (auto cmpiOp = mlir::dyn_cast<mlir::arith::CmpIOp>(op)) {
         auto op1 = cmpiOp.getOperand(0);
         auto op2 = cmpiOp.getOperand(1);
         const auto &cmpi_vec = builder.create<mlir::arith::CmpIOp>(cmpiOp->getLoc(), cmpiOp.getPredicate(), map[op1.getDefiningOp()], map[op2.getDefiningOp()]);
         map[cmpiOp.getOperation()] = cmpi_vec->getResult(0);
+        // collect garbage
+        garbage.push_back(cmpiOp.getOperation());
     } else if (auto extuiOp = mlir::dyn_cast<mlir::arith::ExtUIOp>(op)) {
         auto op1 = extuiOp.getOperand();
         auto vectorType = mlir::VectorType::get({8}, builder.getIntegerType(32));
         const auto &extui_vec = builder.create<mlir::arith::ExtUIOp>(extuiOp->getLoc(), vectorType, map[op1.getDefiningOp()]);
         map[extuiOp.getOperation()] = extui_vec->getResult(0);
+        // collect garbage
+        garbage.push_back(extuiOp.getOperation());
     } else if (auto muliOp = mlir::dyn_cast<mlir::arith::MulIOp>(op)) {
         auto op1 = muliOp.getOperand(0);
         auto op2 = muliOp.getOperand(1);
         const auto &muli_vec = builder.create<mlir::arith::MulIOp>(muliOp->getLoc(), map[op1.getDefiningOp()], map[op2.getDefiningOp()]);
         map[muliOp.getOperation()] = muli_vec->getResult(0);
+        // collect garbage
+        garbage.push_back(muliOp.getOperation());
     } else if (auto subiOp = mlir::dyn_cast<mlir::arith::SubIOp>(op)) {
         auto op1 = subiOp.getOperand(0);
         auto op2 = subiOp.getOperand(1);
         const auto &subi_vec = builder.create<mlir::arith::SubIOp>(subiOp->getLoc(), map[op1.getDefiningOp()], map[op2.getDefiningOp()]);
         map[subiOp.getOperation()] = subi_vec->getResult(0);
+        // collect garbage
+        garbage.push_back(subiOp.getOperation());
     } else if (auto selectOp = mlir::dyn_cast<mlir::arith::SelectOp>(op)) {
         auto op1 = selectOp.getOperand(0);
         auto op2 = selectOp.getOperand(1);
         auto op3 = selectOp.getOperand(2);
         const auto &select_vec = builder.create<mlir::arith::SelectOp>(selectOp->getLoc(), map[op1.getDefiningOp()], map[op2.getDefiningOp()], map[op3.getDefiningOp()]);
         map[selectOp.getOperation()] = select_vec->getResult(0);
+        // collect garbage
+        garbage.push_back(selectOp.getOperation());
     } else if (auto storeOp = mlir::dyn_cast<mlir::affine::AffineStoreOp>(op)) {
         auto memRef = storeOp.getMemRef(); 
         // auto vectorType = mlir::VectorType::get({8}, builder.getIntegerType(32));
@@ -69,6 +89,8 @@ void vectorize_ops(std::unordered_map<mlir::Operation *, mlir::OpResult> &map, s
         auto op = storeOp.getValueToStore();
         builder.create<mlir::vector::StoreOp>(storeOp->getLoc(), map[op.getDefiningOp()], memRef, indices);
         // map[storeOp.getOperation()] = store_vec->getResult(0);
+        // collect garbage
+        storeOp.erase();
     } 
 } 
 
@@ -78,9 +100,9 @@ struct GenSeqPass : public mlir::PassWrapper<GenSeqPass, mlir::OperationPass<mli
         // map to store hoist ops
         getOperation().walk([](mlir::affine::AffineForOp forOp1) {
             std::unordered_map<mlir::Operation *, mlir::OpResult> map;
-            std::vector<mlir::Operation *> garbage;
             forOp1.getBody()->walk([&](mlir::affine::AffineForOp forOp2) {
                 forOp2.getBody()->walk([&](mlir::affine::AffineForOp forOp3) {
+                    std::vector<mlir::Operation *> garbage;
                     mlir::OpBuilder builder(forOp1->getContext());
                     builder.setInsertionPointToStart(forOp1->getBlock());
                     forOp1.setStep(8);
@@ -108,13 +130,10 @@ struct GenSeqPass : public mlir::PassWrapper<GenSeqPass, mlir::OperationPass<mli
                         llvm::outs() << "vectorizing\n";
                         vectorize_ops(map, garbage, op, builder);
                     });
+                    collect_garbage(garbage);
+                    llvm::outs() << "Garbage left: " << garbage.size() << "\n";
                 });
             });
-            llvm::outs() << "Collecting Garbage\n";
-
-            for (auto &g: garbage) {
-                g->remove();
-            }
         });
     }
 };
--- Pass/opt.mlir
+++ Pass/opt.mlir
@@ -1,44 +1,13 @@
-run gen seq pass
-Collecting Garbage
-Collecting Garbage
-Collecting Garbage
-Collecting Garbage
-vectorizing
-vectorizing
-vectorizing
-vectorizing
-vectorizing
-vectorizing
-vectorizing
-vectorizing
-vectorizing
-vectorizing
-vectorizing
-vectorizing
-vectorizing
-vectorizing
-vectorizing
-vectorizing
-vectorizing
-vectorizing
-Collecting Garbage
-Collecting Garbage
-Collecting Garbage
-Collecting Garbage
-Collecting Garbage
 module {
   llvm.mlir.global internal constant @str0("Success!\0A\00") {addr_space = 0 : i32}
   llvm.func @printf(!llvm.ptr, ...) -> i32
-  func.func @Needleman_Wunsch(%arg0: memref<?x50xi32>, %arg1: memref<?x50xi32>, %arg2: memref<?x51x512xi32>) attributes {llvm.linkage = #llvm.linkage<external>} {
+  func.func @Needleman_Wunsch(%arg0: memref<?x512xi32>, %arg1: memref<?x512xi32>, %arg2: memref<?x51x512xi32>) attributes {llvm.linkage = #llvm.linkage<external>} {
     %c-5_i32 = arith.constant -5 : i32
     %0 = vector.broadcast %c-5_i32 : i32 to vector<8xi32>
     %c7_i32 = arith.constant 7 : i32
     %1 = vector.broadcast %c7_i32 : i32 to vector<8xi32>
     %c2_i32 = arith.constant 2 : i32
     %2 = vector.broadcast %c2_i32 : i32 to vector<8xi32>
-    %c-5_i32_0 = arith.constant -5 : i32
-    %c2_i32_1 = arith.constant 2 : i32
-    %c7_i32_2 = arith.constant 7 : i32
     %c0_i32 = arith.constant 0 : i32
     affine.for %arg3 = 0 to 512 {
       affine.for %arg4 = 0 to 51 {
@@ -56,8 +25,8 @@ module {
           %5 = vector.load %arg2[%arg4, %arg5, %arg3] : memref<?x51x512xi32>, vector<8xi32>
           %6 = arith.addi %5, %0 : vector<8xi32>
           %7 = vector.load %arg2[%arg4, %arg5, %arg3] : memref<?x51x512xi32>, vector<8xi32>
-          %8 = vector.load %arg0[%arg3, %arg4] : memref<?x50xi32>, vector<8xi32>
-          %9 = vector.load %arg1[%arg3, %arg4] : memref<?x50xi32>, vector<8xi32>
+          %8 = vector.load %arg0[%arg4, %arg3] : memref<?x512xi32>, vector<8xi32>
+          %9 = vector.load %arg1[%arg4, %arg3] : memref<?x512xi32>, vector<8xi32>
           %10 = arith.cmpi eq, %8, %9 : vector<8xi32>
           %11 = arith.extui %10 : vector<8xi1> to vector<8xi32>
           %12 = arith.muli %11, %1 : vector<8xi32>
@@ -68,23 +37,6 @@ module {
           %17 = arith.cmpi slt, %16, %14 : vector<8xi32>
           %18 = arith.select %17, %14, %16 : vector<8xi1>, vector<8xi32>
           vector.store %18, %arg2[%arg4, %arg5, %arg3] : memref<?x51x512xi32>, vector<8xi32>
-          %19 = affine.load %arg2[%arg4 - 1, %arg5, %arg3] : memref<?x51x512xi32>
-          %20 = arith.addi %19, %c-5_i32_0 : i32
-          %21 = affine.load %arg2[%arg4, %arg5 - 1, %arg3] : memref<?x51x512xi32>
-          %22 = arith.addi %21, %c-5_i32_0 : i32
-          %23 = affine.load %arg2[%arg4 - 1, %arg5 - 1, %arg3] : memref<?x51x512xi32>
-          %24 = affine.load %arg0[%arg3, %arg4 - 1] : memref<?x50xi32>
-          %25 = affine.load %arg1[%arg3, %arg4 - 1] : memref<?x50xi32>
-          %26 = arith.cmpi eq, %24, %25 : i32
-          %27 = arith.extui %26 : i1 to i32
-          %28 = arith.muli %27, %c7_i32_2 : i32
-          %29 = arith.subi %23, %28 : i32
-          %30 = arith.addi %29, %c2_i32_1 : i32
-          %31 = arith.cmpi slt, %20, %22 : i32
-          %32 = arith.select %31, %22, %20 : i32
-          %33 = arith.cmpi slt, %32, %30 : i32
-          %34 = arith.select %33, %30, %32 : i32
-          affine.store %34, %arg2[%arg4, %arg5, %arg3] : memref<?x51x512xi32>
         }
       }
     }
@@ -97,8 +49,8 @@ module {
     %c3_i32 = arith.constant 3 : i32
     %c0_i32 = arith.constant 0 : i32
     %alloca = memref.alloca() : memref<51x51x512xi32>
-    %alloca_0 = memref.alloca() : memref<512x50xi32>
-    %alloca_1 = memref.alloca() : memref<512x50xi32>
+    %alloca_0 = memref.alloca() : memref<50x512xi32>
+    %alloca_1 = memref.alloca() : memref<50x512xi32>
     affine.for %arg0 = 0 to 512 {
       %3 = arith.index_cast %arg0 : index to i32
       %4 = arith.muli %3, %c3_i32 : i32
@@ -108,7 +60,7 @@ module {
         %7 = arith.addi %6, %4 : i32
         %8 = arith.addi %7, %5 : i32
         %9 = arith.remsi %8, %c4_i32 : i32
-        affine.store %9, %alloca_1[%arg0, %arg1] : memref<512x50xi32>
+        affine.store %9, %alloca_1[%arg1, %arg0] : memref<50x512xi32>
       }
     }
     affine.for %arg0 = 0 to 512 {
@@ -122,13 +74,13 @@ module {
         %9 = arith.muli %5, %c2_i32 : i32
         %10 = arith.addi %8, %9 : i32
         %11 = arith.remsi %10, %c4_i32 : i32
-        affine.store %11, %alloca_0[%arg0, %arg1] : memref<512x50xi32>
+        affine.store %11, %alloca_0[%arg1, %arg0] : memref<50x512xi32>
       }
     }
-    %cast = memref.cast %alloca_1 : memref<512x50xi32> to memref<?x50xi32>
-    %cast_2 = memref.cast %alloca_0 : memref<512x50xi32> to memref<?x50xi32>
+    %cast = memref.cast %alloca_1 : memref<50x512xi32> to memref<?x512xi32>
+    %cast_2 = memref.cast %alloca_0 : memref<50x512xi32> to memref<?x512xi32>
     %cast_3 = memref.cast %alloca : memref<51x51x512xi32> to memref<?x51x512xi32>
-    call @Needleman_Wunsch(%cast, %cast_2, %cast_3) : (memref<?x50xi32>, memref<?x50xi32>, memref<?x51x512xi32>) -> ()
+    call @Needleman_Wunsch(%cast, %cast_2, %cast_3) : (memref<?x512xi32>, memref<?x512xi32>, memref<?x51x512xi32>) -> ()
     %0 = llvm.mlir.addressof @str0 : !llvm.ptr
     %1 = llvm.getelementptr %0[0, 0] : (!llvm.ptr) -> !llvm.ptr, !llvm.array<10 x i8>
     %2 = llvm.call @printf(%1) vararg(!llvm.func<i32 (ptr, ...)>) : (!llvm.ptr) -> i32
